99a819295ff3486070f3070c52de776c93746bf0
Vishnu S <myworkspacexx@gmail.com>
2025-11-02 17:11:58 +0530

    Implement username validation and fetch user details
    Added username validation and user details fetching functionality.

--- script.js
+++ script.js
@@ -10,11 +10,47 @@ document.addEventListener("DOMContentLoaded",function(){
   let hardLabel= document.querySelector('.hard-label');
   let statCards= document.querySelector('.stats-cards');
 
-  
+  // validating username
 
+    function validateUsername(userName){
+      // check whether username is empty
+      if(userName.trim() === ''){
+        alert('Username should not be empty');
+        return false;
+      }
+
+      //regex comparison
+      const usernameRegex = /^[a-zA-Z0-9_-]{1,15}$/;
+      const isMatching = usernameRegex.test(userName);     
+      if(!isMatching){
+        alert('Enter Valid Username');
+        return false;
+      }
+      else{
+        searchBtn.innerHTML='<button disabled>Searching</button>'
+        return true;
+      }
+    }
+
+// fetching user details using username
+
+async function fetchUserDetails(userName){
+  const url = `https://leetcode-stats-api.herokuapp.com/${userName}`;
+  
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
+}
+
+
+//when clicks on search button
 
   searchBtn.addEventListener('click',function(){
     const userName = input.value;
-    console.log(userName);
+    validateUsername(userName);
+    if(validateUsername(userName)){
+      fetchUserDetails(userName);
+    }
   })
+
 })
